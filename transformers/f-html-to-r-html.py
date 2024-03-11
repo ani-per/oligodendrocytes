a656d0151ccdffd3c21ba29b7c911aa11b7ca3e3
amp-gh <amp7226@psu.edu>
2024-03-11 14:49:22 -0400

    python3 formatting

--- transformers/f-html-to-r-html.py
+++ transformers/f-html-to-r-html.py
@@ -18,79 +18,96 @@ from htmlparser import LastNParser
 
 
 import codecs
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+sys.stdout = codecs.getwriter("utf8")(sys.stdout)
+sys.stderr = codecs.getwriter("utf8")(sys.stderr)
 
 filename_in = sys.argv[1]
-filename_out = filename_in.replace('.f.', '.r.')
+filename_out = filename_in.replace(".f.", ".r.")
 
-with io.open(filename_in, 'r', encoding='utf-8') as file_in:
-	contents = file_in.read()
-	#sys.stderr.write('\n\n' + filename_in + '\n\n')
+with io.open(filename_in, "r", encoding="utf-8") as file_in:
+    contents = file_in.read()
+    # sys.stderr.write('\n\n' + filename_in + '\n\n')
 
 if 1:
-	# default
-	use_tags = False
-	use_paren_quote = True
+    # default
+    use_tags = False
+    use_paren_quote = True
 if 0:
-	# eft
-	use_tags = True
-	use_paren_quote = False
+    # eft
+    use_tags = True
+    use_paren_quote = False
 
-EXTRA_TAGS   = '(?:<\/?b>|)'
-PG_TAG_S     = '<span\ class=\"s\d\">'
-PG_TAG_E     = '<\/span>'
-SPACE        = u'[  ]'
-SPACES       = SPACE + '+'
-SPACEM       = SPACE + '*'
-SPACE_NONBSP = u'[ ]'
-SPACES_NONBSP= SPACE_NONBSP + '+'
+EXTRA_TAGS = "(?:<\/?b>|)"
+PG_TAG_S = '<span\ class="s\d">'
+PG_TAG_E = "<\/span>"
+SPACE = "[  ]"
+SPACES = SPACE + "+"
+SPACEM = SPACE + "*"
+SPACE_NONBSP = "[ ]"
+SPACES_NONBSP = SPACE_NONBSP + "+"
 
 if use_paren_quote:
-	QUOTE_S = u'“'
-	QUOTE_E = u'”'
-	PG_BRACKET_S = u'[\(]' + QUOTE_S
-	PG_BRACKET_E = QUOTE_E + u'[\)]'
-	PG_MIDDLE    = u'[^\)]+'
+    QUOTE_S = "“"
+    QUOTE_E = "”"
+    PG_BRACKET_S = "[\(]" + QUOTE_S
+    PG_BRACKET_E = QUOTE_E + "[\)]"
+    PG_MIDDLE = "[^\)]+"
 else:
-	QUOTE_S = u''
-	QUOTE_E = u''
-	PG_BRACKET_S = '[\(\[]'
-	PG_BRACKET_E = '[\)\]]'
-	PG_MIDDLE    = '[^\)\]]+'
+    QUOTE_S = ""
+    QUOTE_E = ""
+    PG_BRACKET_S = "[\(\[]"
+    PG_BRACKET_E = "[\)\]]"
+    PG_MIDDLE = "[^\)\]]+"
 
-PG_OR = QUOTE_E + r' or ' + QUOTE_S
-PG_BRACKET_INSTRUCTION = '[\[\(]' + u'(?P<in>emphasize[^\]\)]*|pause|read slowly[^\]\)]*)' + '[\]\)]'
+PG_OR = QUOTE_E + r" or " + QUOTE_S
+PG_BRACKET_INSTRUCTION = (
+    "[\[\(]" + "(?P<in>emphasize[^\]\)]*|pause|read slowly[^\]\)]*)" + "[\]\)]"
+)
 
 if use_tags:
-	PG_SB = u'(?P<ss>' + SPACES       + ')' + \
-	                     PG_TAG_S + EXTRA_TAGS + \
-	         '(?P<sb>' + PG_BRACKET_S + ')'
-	PG_M  = u'(?P<m>'  + PG_MIDDLE    + ')'
-	PG_EB = u'(?P<eb>' + PG_BRACKET_E + ')' + \
-	                     EXTRA_TAGS + PG_TAG_E + \
-	         '(?P<es>' + SPACEM       + ')'
+    PG_SB = (
+        "(?P<ss>"
+        + SPACES
+        + ")"
+        + PG_TAG_S
+        + EXTRA_TAGS
+        + "(?P<sb>"
+        + PG_BRACKET_S
+        + ")"
+    )
+    PG_M = "(?P<m>" + PG_MIDDLE + ")"
+    PG_EB = (
+        "(?P<eb>"
+        + PG_BRACKET_E
+        + ")"
+        + EXTRA_TAGS
+        + PG_TAG_E
+        + "(?P<es>"
+        + SPACEM
+        + ")"
+    )
 
-	PGB = PG_SB + PG_M + PG_EB
+    PGB = PG_SB + PG_M + PG_EB
 
-	PG_BRACKET_INSTRUCTION = PG_TAG_S + EXTRA_TAGS + PG_BRACKET_INSTRUCTION + EXTRA_TAGS + PG_TAG_E + '\s'
+    PG_BRACKET_INSTRUCTION = (
+        PG_TAG_S + EXTRA_TAGS + PG_BRACKET_INSTRUCTION + EXTRA_TAGS + PG_TAG_E + "\s"
+    )
 
 else:
-	PG_SB = u'(?P<ss>' + SPACES       + ')' + \
-	         '(?P<sb>' + PG_BRACKET_S + ')'
-	PG_M  = u'(?P<m>'  + PG_MIDDLE    + ')'
-	PG_EB = u'(?P<eb>' + PG_BRACKET_E + ')' + \
-	         '(?P<es>' + SPACEM       + ')'
+    PG_SB = "(?P<ss>" + SPACES + ")" + "(?P<sb>" + PG_BRACKET_S + ")"
+    PG_M = "(?P<m>" + PG_MIDDLE + ")"
+    PG_EB = "(?P<eb>" + PG_BRACKET_E + ")" + "(?P<es>" + SPACEM + ")"
 
-	PGB = PG_SB + PG_M + PG_EB
+    PGB = PG_SB + PG_M + PG_EB
 
-	PG_BRACKET_INSTRUCTION = PG_BRACKET_INSTRUCTION + '\s'
+    PG_BRACKET_INSTRUCTION = PG_BRACKET_INSTRUCTION + "\s"
 
 
 # TODO eventually boundary symbols like “”‘’ can be left out of rb
 # TODO remove </b> <b> at end
 
-fake_contents = u'''--
+fake_contents = """--
 test aaa     <span class="s1"><b>[bbb]</b></span>      test
 test aaa</b> <span class="s1"><b>[bbb]</b></span>   <b>test
 test aaa</i> <span class="s1"><b>[bbb]</b></span>   <i>test
@@ -127,122 +144,131 @@ solo <span class="s1"><b>(read slowly)</b></span> <span class="s2"></span>long
 ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> (“lay-AH-koh-on”) <i>and His Sons</i>
 ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> <i>(“lay-AH-koh-on”) and His Sons</i>
 ANSWER: <span class="s1"><b><i>Laocoön</i></b></span><i> (“lay-AH-koh-on”) and His Sons</i>
-'''
+"""
 
-zz=1
-ruby_tag_color = '\033[107;4m'*zz
-contents_color = '\033[102;4m'*zz
-bracket_color  = '\033[103;4m'*zz
-space_color    = '\033[103;4m'*zz
-reset_color    = '\033[0m'*zz
+zz = 1
+ruby_tag_color = "\033[107;4m" * zz
+contents_color = "\033[102;4m" * zz
+bracket_color = "\033[103;4m" * zz
+space_color = "\033[103;4m" * zz
+reset_color = "\033[0m" * zz
+
 
 def wrap_caps(b):
-	return re.sub('[A-Z]{2,}', '<span class="pg-stress">\g<0></span>', b)
+    return re.sub("[A-Z]{2,}", '<span class="pg-stress">\g<0></span>', b)
+
 
 def middot(b):
-	return re.sub('-', u'·', b)
+    return re.sub("-", "·", b)
+
 
 def word_count(b):
-	b_first_or_pos = re.search(PG_OR, b)
-	if b_first_or_pos:
-		b = b[:b_first_or_pos.start()]
-	space_count = 1 + len(re.findall(SPACES_NONBSP, b))
-	return space_count
+    b_first_or_pos = re.search(PG_OR, b)
+    if b_first_or_pos:
+        b = b[: b_first_or_pos.start()]
+    space_count = 1 + len(re.findall(SPACES_NONBSP, b))
+    return space_count
+
 
 def bracket_instruction(b):
-	return re.sub(PG_BRACKET_INSTRUCTION, u'<small class="bracket-instruction">(\g<in>)</small> ', b)
+    return re.sub(
+        PG_BRACKET_INSTRUCTION,
+        '<small class="bracket-instruction">(\g<in>)</small> ',
+        b,
+    )
+
 
 def rp_or(b):
-	return re.sub(PG_OR, ' <span class="pg-or">or</span> ', b)
-	# TODO eventually do something like:
-	# <ruby> <rb>...</rb> <rp>(</rp>
-	#  <rt><span>“</span> ... <span>”</span><span> or </span> ...
-	# or completely dispense with all rendered “”
+    return re.sub(PG_OR, ' <span class="pg-or">or</span> ', b)
+    # TODO eventually do something like:
+    # <ruby> <rb>...</rb> <rp>(</rp>
+    #  <rt><span>“</span> ... <span>”</span><span> or </span> ...
+    # or completely dispense with all rendered “”
 
-	# <rp> doesn't work because another <rb> is implied:
-	# return re.sub(PG_OR, '</rt><rp>\g<0></rp><rt>', b)
+    # <rp> doesn't work because another <rb> is implied:
+    # return re.sub(PG_OR, '</rt><rp>\g<0></rp><rt>', b)
+
 
 def html_span_to_ruby(contents):
-	contents = bracket_instruction(contents)
+    contents = bracket_instruction(contents)
 
-	instances = re.finditer(PGB, contents)
-	lastMatch = 0
-	formattedText = ''
+    instances = re.finditer(PGB, contents)
+    lastMatch = 0
+    formattedText = ""
 
-	for match in instances:
-		start, end = match.span()
+    for match in instances:
+        start, end = match.span()
 
-		prev = contents[lastMatch : start]
-		main = contents[start : end]
+        prev = contents[lastMatch:start]
+        main = contents[start:end]
 
-		a = 'nothing yet'
+        a = "nothing yet"
 
-		ss = match.group('ss')
-		sb = match.group('sb')
-		b  = match.group('m')
-		eb = match.group('eb')
-		es = match.group('es')
+        ss = match.group("ss")
+        sb = match.group("sb")
+        b = match.group("m")
+        eb = match.group("eb")
+        es = match.group("es")
 
-		b_word_count = word_count(b)
+        b_word_count = word_count(b)
 
-		# b = b_middot = middot(b)
-		b = b_rp_or = rp_or(b)
-		# b = b_wrap_caps = wrap_caps(b)
+        # b = b_middot = middot(b)
+        b = b_rp_or = rp_or(b)
+        # b = b_wrap_caps = wrap_caps(b)
 
-		last_newline_pos = prev.rfind('\n') + 1
-		prev1 = prev[:last_newline_pos]
-		prev2 = prev[last_newline_pos:]
-		prev2a, a, closing_tags = real_a = LastNParser(prev2).last_n_words(b_word_count)
+        last_newline_pos = prev.rfind("\n") + 1
+        prev1 = prev[:last_newline_pos]
+        prev2 = prev[last_newline_pos:]
+        prev2a, a, closing_tags = real_a = LastNParser(prev2).last_n_words(b_word_count)
 
-		# for caver stuff only
-		# a_stripped = unidecode(re.sub('<[^<]+?>', '', a))
-		# a_phonetic = caverphone(a_stripped)
-		# b_stripped = unidecode(re.sub('<[^<]+?>', '', b))
-		# b_phonetic = caverphone(b_stripped)
-		# distance = Levenshtein.distance(a_phonetic, b_phonetic)
-		# ratio = Levenshtein.ratio(a_phonetic, b_phonetic)
-		# sys.stderr.write( '%-20s\t%-12s\t%-36s\t%-12s\t%2d\t%0.2f\n' % (a_stripped, a_phonetic, b_stripped, b_phonetic, distance, ratio) )
+        # for caver stuff only
+        # a_stripped = unidecode(re.sub('<[^<]+?>', '', a))
+        # a_phonetic = caverphone(a_stripped)
+        # b_stripped = unidecode(re.sub('<[^<]+?>', '', b))
+        # b_phonetic = caverphone(b_stripped)
+        # distance = Levenshtein.distance(a_phonetic, b_phonetic)
+        # ratio = Levenshtein.ratio(a_phonetic, b_phonetic)
+        # sys.stderr.write( '%-20s\t%-12s\t%-36s\t%-12s\t%2d\t%0.2f\n' % (a_stripped, a_phonetic, b_stripped, b_phonetic, distance, ratio) )
 
-		ap = ' '*(41-len(a))
-		bp = ' '*(41-len(b))
-		def h(a):
-			return a
-		ruby_tuples = [
-			(             '' , h('<ruby>')       ),
-			( ruby_tag_color , h('<rb>')         ),
-			( contents_color , a              ),
-			( ruby_tag_color , h('</rb>')        ),
-			( reset_color+ap , ''             ),
-			( ruby_tag_color , h('<rp>')         ),
-			(    space_color , ss             ),
-			(  bracket_color , sb             ),
-			( ruby_tag_color , h('</rp><rt>')    ),
-			( contents_color , b              ),
-			( ruby_tag_color , h('</rt><rp>')    ),
-			(  bracket_color , eb             ),
-			( ruby_tag_color , h('</rp>')        ),
-			( reset_color+bp , h('</ruby>')      ),
-			(  bracket_color , closing_tags   ),
-			(    space_color , es             ),
-			(    reset_color , ''             ),
-		]
-		ruby_str       = ''.join([txt     for clr,txt in ruby_tuples])
-		ruby_str_color = ''.join([clr+txt for clr,txt in ruby_tuples])
+        ap = " " * (41 - len(a))
+        bp = " " * (41 - len(b))
 
-		formattedText += (
-			prev1 +
-			prev2a +
-			ruby_str
-		)
-		sys.stderr.write(ruby_str_color + "\n")
+        def h(a):
+            return a
 
-		lastMatch = end
-	formattedText += contents[lastMatch:]
-	return formattedText
+        ruby_tuples = [
+            ("", h("<ruby>")),
+            (ruby_tag_color, h("<rb>")),
+            (contents_color, a),
+            (ruby_tag_color, h("</rb>")),
+            (reset_color + ap, ""),
+            (ruby_tag_color, h("<rp>")),
+            (space_color, ss),
+            (bracket_color, sb),
+            (ruby_tag_color, h("</rp><rt>")),
+            (contents_color, b),
+            (ruby_tag_color, h("</rt><rp>")),
+            (bracket_color, eb),
+            (ruby_tag_color, h("</rp>")),
+            (reset_color + bp, h("</ruby>")),
+            (bracket_color, closing_tags),
+            (space_color, es),
+            (reset_color, ""),
+        ]
+        ruby_str = "".join([txt for clr, txt in ruby_tuples])
+        ruby_str_color = "".join([clr + txt for clr, txt in ruby_tuples])
+
+        formattedText += prev1 + prev2a + ruby_str
+        sys.stderr.write(ruby_str_color + "\n")
+
+        lastMatch = end
+    formattedText += contents[lastMatch:]
+    return formattedText
+
 
 fake = False
 if fake:
-	out = html_span_to_ruby(fake_contents)
+    out = html_span_to_ruby(fake_contents)
 else:
-	out = html_span_to_ruby(contents)
+    out = html_span_to_ruby(contents)
 sys.stdout.write(out)
--- transformers/htmlparser.py
+++ transformers/htmlparser.py
@@ -25,102 +25,106 @@ import re
 
 # Further documentation: https://minkowski.space/quizbowl/pronouncing-dictionary/writing-pgs.html#special
 
-SPACES = u'[  –‒]+'
+SPACES = "[  –‒]+"
+
 
 class UnbalancedError(Exception):
-	pass
+    pass
+
 
 # TODO power marks (*)
 # TODO handle 'foo</b> bar'
 
+
 class LastNParser(HTMLParser):
-	def __init__(self, contents):
-		self.contents = contents
-		self.end = len(contents)
-		self.ls = []
-		self.reset()
-		self.feed(contents)
+    def __init__(self, contents):
+        self.contents = contents
+        self.end = len(contents)
+        self.ls = []
+        self.reset()
+        self.feed(contents)
 
-	def handle_starttag(self, tag, attrs):
-		self.ls.append(('start', self.getpos()[1], tag, attrs ))
+    def handle_starttag(self, tag, attrs):
+        self.ls.append(("start", self.getpos()[1], tag, attrs))
 
-	def handle_endtag(self, tag):
-		self.ls.append(('end',   self.getpos()[1], tag        ))
+    def handle_endtag(self, tag):
+        self.ls.append(("end", self.getpos()[1], tag))
 
-	def handle_data(self, data):
-		self.ls.append(('data',  self.getpos()[1], data       ))
+    def handle_data(self, data):
+        self.ls.append(("data", self.getpos()[1], data))
 
-	def last_n_words(self, n):
-		(x, y) = self.start_pos_of_nth_last_word(n)
-		last_n_words = (self.contents[:x], self.contents[x:y], self.contents[y:])
-		return last_n_words
+    def last_n_words(self, n):
+        (x, y) = self.start_pos_of_nth_last_word(n)
+        last_n_words = (self.contents[:x], self.contents[x:y], self.contents[y:])
+        return last_n_words
 
-	def start_pos_of_nth_last_word(self, n):
-		ls = self.ls
-		count = 0
-		stack = [(self.end, '')]
-		done  = False
+    def start_pos_of_nth_last_word(self, n):
+        ls = self.ls
+        count = 0
+        stack = [(self.end, "")]
+        done = False
 
-		if n == 0:
-			done = self.end
+        if n == 0:
+            done = self.end
 
-		for i, token in reversed(list(enumerate(ls))):
-			if done:
-				break
+        for i, token in reversed(list(enumerate(ls))):
+            if done:
+                break
 
-			if token[0] == 'end':
-				stack.append(token[1:3])
+            if token[0] == "end":
+                stack.append(token[1:3])
 
-			elif token[0] == 'start':
-				if len(stack) == 1:
-					# raise UnbalancedError(token[2], self.contents)
-					# TODO deal with string that ends with start tag
-					try:
-						done = ls[i + 1][1]
-					except IndexError:
-						done = ''
-					break
-				elif stack[-1][1] == token[2]:
-					stack.pop()
-				else:
-					raise UnbalancedError(token[2], self.contents)
+            elif token[0] == "start":
+                if len(stack) == 1:
+                    # raise UnbalancedError(token[2], self.contents)
+                    # TODO deal with string that ends with start tag
+                    try:
+                        done = ls[i + 1][1]
+                    except IndexError:
+                        done = ""
+                    break
+                elif stack[-1][1] == token[2]:
+                    stack.pop()
+                else:
+                    raise UnbalancedError(token[2], self.contents)
 
-			elif token[0] == 'data':
-				# TODO need unicode flag?
-				fi = re.finditer(SPACES, token[2])
-				for matched_space in reversed(list(fi)):
-					count += 1
+            elif token[0] == "data":
+                # TODO need unicode flag?
+                fi = re.finditer(SPACES, token[2])
+                for matched_space in reversed(list(fi)):
+                    count += 1
 
-					if count >= n:
-						done = token[1] + matched_space.end()
-						break
+                    if count >= n:
+                        done = token[1] + matched_space.end()
+                        break
 
-		if not done:
-			done = 0
+        if not done:
+            done = 0
 
-		return (done, stack[-1][0])
+        return (done, stack[-1][0])
 
-if 1 and __name__ == '__main__':
-	zz=1
-	colorCmd = '\033[107;4m'*zz
-	resetCmd = '\033[0m'*zz
 
-	# parser = LastNParser('One morning, when <i>Gregor Samsa</i>')
-	test = u'Luis Buñuel, <i>L’Âge d’Or</i> (“lodge dor”). For 10'
-	test = u'power after overcoming Xiàng Yǔ’s</b> <span class="s2"><b>[shyong yoo’s]</b></span> <b>state of (*)</b> Chu.'
-	tests = [
-		u'-1 0 1</i> 2</b>',
-		u'-1 0 1</b> 2',
-		u'-1 0</b> 1 2',
-		u'-1 0 <b>1 2',
-		u'ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> <i>word',
-		u'ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> <i>'
-	]
-	for test in tests:
-		parser = LastNParser(test)
-		for i in range(3):
-			x,y,z = parser.last_n_words(i)
-			print()
-			print(i, colorCmd + x + resetCmd)
-			print(i, ' '*len(x) + colorCmd + y + resetCmd)
-			print(i, ' '*len(x+y) + colorCmd + z + resetCmd)
+if 1 and __name__ == "__main__":
+    zz = 1
+    colorCmd = "\033[107;4m" * zz
+    resetCmd = "\033[0m" * zz
+
+    # parser = LastNParser('One morning, when <i>Gregor Samsa</i>')
+    test = "Luis Buñuel, <i>L’Âge d’Or</i> (“lodge dor”). For 10"
+    test = 'power after overcoming Xiàng Yǔ’s</b> <span class="s2"><b>[shyong yoo’s]</b></span> <b>state of (*)</b> Chu.'
+    tests = [
+        "-1 0 1</i> 2</b>",
+        "-1 0 1</b> 2",
+        "-1 0</b> 1 2",
+        "-1 0 <b>1 2",
+        'ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> <i>word',
+        'ANSWER: <span class="s1"><b><i>Laocoön</i></b></span> <i>',
+    ]
+    for test in tests:
+        parser = LastNParser(test)
+        for i in range(3):
+            x, y, z = parser.last_n_words(i)
+            print()
+            print(i, colorCmd + x + resetCmd)
+            print(i, " " * len(x) + colorCmd + y + resetCmd)
+            print(i, " " * len(x + y) + colorCmd + z + resetCmd)
